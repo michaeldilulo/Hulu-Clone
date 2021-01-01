378b2839e134ca45060fab7db59b17e4b847aa9d
michael dilulo <snoleopard01001101@gmail.com>
2021-01-01 04:02:08 -0800

    Markup for Application

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-<h1>Hulu Clone</h1>
+      {/* Header */}
+
+      {/* Sub Navigation Bar */}
+
+      {/* Results */}
     </div>
   );
 }
