4d0187652256c1b17436b8be515c4157412f73a2
michael dilulo <snoleopard01001101@gmail.com>
2021-01-04 10:24:05 -0800

    Action / Comedy : Requests

--- src/requests.js
+++ src/requests.js
@@ -2,5 +2,7 @@ const API_KEY = "3d823ae7290401fbb896a698daf49a78";
 
 export default {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie/api_key=${API_KEY}&with_genres=35`
 }
