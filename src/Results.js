390ba32b5ea178c76645c3419bb92337860b3c12
michael dilulo <snoleopard01001101@gmail.com>
2021-01-09 12:24:34 -0800

    Start of Axios get request w/ useEffect hook

--- src/Results.js
+++ src/Results.js
@@ -1,10 +1,20 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import "./Results.css"
 import VideoCard from "./VideoCard"
+import axios from './axios'
+import requests from "./requests"
 
 
 function Results() {
     const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+        }
+
+        fetchData();
+    }, [])
+
     return (
         <div className="results">
             {movies.map((movie) => (
