083f96c10a3d62f59bb14b66ea2fc371b9fb495c
michael dilulo <snoleopard01001101@gmail.com>
2021-01-11 10:03:13 -0800

    Selected Option prop and request

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 import Header from "./Header"
 import Nav from "./Nav"
 import Results from "./Results"
+import { useState } from "react"
+import requests from "./requests"
 
 function App() {
+const [selectedOption, setSelectedOption] = useState(requests.fetchTrending)
+
   return (
     <div className="app">
       <Header />
