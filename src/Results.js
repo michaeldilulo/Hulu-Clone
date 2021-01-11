c234f801579940d6f9888c10a0f35eb3dc01d434
michael dilulo <snoleopard01001101@gmail.com>
2021-01-11 10:24:49 -0800

    React Flip Move Implementation

--- src/Results.js
+++ src/Results.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import "./Results.css"
 import VideoCard from "./VideoCard"
 import axios from './axios'
+import FlipMove from "react-flip-move"
 
 
 function Results({selectedOption}) {
@@ -19,9 +20,12 @@ function Results({selectedOption}) {
 
     return (
         <div className="results">
+            <FlipMove>
             {movies.map((movie) => (
                 <VideoCard key={movie.id} movie={movie} />
             ))}
+            </FlipMove>
+
         </div>
     )
 }
--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, {forwardRef} from 'react'
 import "./VideoCard.css"
 import TextTruncate from "react-text-truncate"
-import {ThumbUpSharp} from "@material-ui/icons"
+import { ThumbUpSharp } from "@material-ui/icons"
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
-function VideoCard({movie}) {
+const VideoCard = forwardRef(({movie}, ref) => {
     return (
-        <div className="videoCard">
+        <div ref={ref} className="videoCard">
             <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt="" />
             <TextTruncate
                 line={1}
@@ -23,6 +23,6 @@ function VideoCard({movie}) {
                 {movie.vote_count}</p>
         </div>
     )
-}
+})
 
 export default VideoCard
